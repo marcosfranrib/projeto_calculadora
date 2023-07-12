3c16f8fbc14c8a66e65688f556f29fc3d7df039a
Marcos Francisco Ribeiro <136476694+marcosfranrib@users.noreply.github.com>
2023-07-12 14:52:54 -0300

    .

---
+++ script.js
@@ -0,0 +1,13 @@
+function clicar(){
+    var numero = document.querySelector('.um')
+    var tela = document.getElementById('tela')
+
+    tela.value += numero.innerHTML
+}
+
+function clicar2(){
+    var numero = document.querySelector('.dois')
+    var tela = document.getElementById('tela')
+
+    tela.value += numero.innerHTML
+}
