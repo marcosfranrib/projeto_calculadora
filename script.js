57570c8e94428eb49b33cf8faa04bc56e5d4c580
Marcos Francisco Ribeiro <136476694+marcosfranrib@users.noreply.github.com>
2023-07-13 14:48:33 -0300

    .

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-function clicar(){
-    var numero = document.querySelector('.um')
+function clicar(num){
     var tela = document.getElementById('tela')
+    tela.value += num
 
-    tela.value += numero.innerHTML
 }
 
-function clicar2(){
-    var numero = document.querySelector('.dois')
+function limpar(){
     var tela = document.getElementById('tela')
+    var history = document.getElementById('historico')
+    tela.value =''
+    history.innerHTML = ''
+}
 
-    tela.value += numero.innerHTML
+//quando apertar o botão de somar, armazenar em uma outra variavel
+
+function somar(){
+    
+    var tela = document.getElementById('tela')
+    var history = document.getElementById('historico')
+    var n1 = Number(tela.value)
+
+    if(n1 != ''){
+        tela.value = ''
+        history.innerHTML += ` ${n1} +`
+    }
+
+    var n2 = Number(tela.value)
+
+    if(n2 != ''){
+        tela.value = ''
+        history.innerHTML += ` ${n2} +`
+    }
+
 }
