8eed0f53c69bac896601876090c7e8b0edba9fcf
Marcos Francisco Ribeiro <136476694+marcosfranrib@users.noreply.github.com>
2023-07-14 02:04:49 -0300

    .

--- script.js
+++ script.js
@@ -7,15 +7,21 @@ function clicar(num){
 
 function limpar(){
     var tela = document.getElementById('tela')
-    var history = document.getElementById('historico')
     tela.value =''
-    history.innerHTML = ''
+    
 }
 
 function calculo(){
     let tela = document.getElementById('tela')
     let tela2 = tela.value
-    tela.value = eval(tela)
+
+    if(tela.value == ''){
+        tela.value = ''
+
+    } else {
+       
+        tela.value = eval(tela2)
+    }
 }
 
 function backspace(){
